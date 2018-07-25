9ffed47e3ae1c6ab1292ada57fc1b57dc832f98c
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-07-25 12:21:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+let drivers = ['kirmanj', 'fars', 'web', 'recoded'];
+
+
+const returnFirstTwoDrivers = function (drivers){
+    
+    return drivers.slice(0,2);
+  }
+  
+  const returnLastTwoDrivers = function (drivers){
+    
+    return drivers.slice(drivers.length-2,drivers.length);
+  }
+  
+  
+  const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+
+  const createFareMultiplier = function(num){
+       return function(){
+         return num * num;
+       };
+  }
+   
+  const fareDoubler = function (num){
+        return num + num;
+  }
+   
+  const fareTripler = function (num){
+        return num + num+num;
+  }
+  
+  const selectDifferentDrivers = function(arr, returnLastTwoDrivers){
+    
+    return returnLastTwoDrivers(arr);
+  }
+  
+  
+  
+  
+  
+  // const selectingDrivers= 
+
